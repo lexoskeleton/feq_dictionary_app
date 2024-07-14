700498bca355d2a4805f645c74d3a4ad668f42d4
Iroh-Omolola <irohomolola@gmail.com>
2024-07-14 08:22:36 +0100

    updated the search handle to populate the dictionry results

--- start-here/js/script-main.js
+++ start-here/js/script-main.js
@@ -18,11 +18,59 @@ function handleSearch() {
     input.classList.add("search-box--warning");
   } else {
     warningMessage.style.display = "none";
-    input.classList.remove("search-box--warning");
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`)
       .then((response) => response.json())
       .then((data) => {
-       console.log("data", data)
+        if (data.title) {
+          document.getElementById(
+            "title-wrapper"
+          ).innerHTML = `<p>No definition found for <strong>${input.value}</strong>.</p>`;
+        } else {
+          const meanings = data[0].meanings;
+          const phonetics = data[0].phonetics;
+
+          document.querySelector(".title").innerText = input.value;
+
+          phonetics.forEach((phonetic) => {
+            if (phonetic.text) {
+              const h2 = document.querySelector("h2");
+              h2.innerText = phonetic.text;
+
+              const audio = document.querySelector("audio");
+              audio.src = phonetic.audio;
+            }
+          });
+
+          const resultWrappers = document.querySelectorAll(".result-wrapper");
+          resultWrappers.forEach((resultWrapper, index) => {
+            const meaning = meanings[index];
+            if (meaning) {
+              const gramType = resultWrapper.querySelector(
+                ".gramitical-type_wrapper h3"
+              );
+              gramType.innerText = meaning.partOfSpeech;
+
+              const meaningResultWrapper = resultWrapper.querySelector(
+                ".meaning-result_wrapper"
+              );
+              const listOfMeanings =
+                meaningResultWrapper.querySelector(".list-of-meanings");
+              listOfMeanings.innerHTML = "";
+
+              meaning.definitions.forEach((definition) => {
+                const li = document.createElement("li");
+                li.innerText = definition.definition;
+                listOfMeanings.appendChild(li);
+              });
+
+            }
+          });
+
+          // Update source
+          const sourceDiv = document.querySelector("div > h4 + a");
+          sourceDiv.innerText = data[0].sourceUrls;
+          sourceDiv.href = data[0].sourceUrls;
+        }
       })
       .catch((error) => {
         document.getElementById(
