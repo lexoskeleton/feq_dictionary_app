d51ae4238efccb012945248b860515e52abde910
knopgm <65956162+knopgm@users.noreply.github.com>
2024-07-26 17:20:53 +0200

    [Issue #23] Refactoring Search Box input styles and HTML structure + prepare search value for the API call (#32)
    * Refactor search box styles and html structure
    
    * fix: fix style border radius
    
    ---------
    
    Co-authored-by: Ainur Saduova <112205437+NuraSad@users.noreply.github.com>

--- start-here/js/script-main.js
+++ start-here/js/script-main.js
@@ -1,21 +1,24 @@
 //Message to cheange class if input is empty:
-const warningMessage = document.querySelector(".warning-message-hidden");
+const warningMessage = document.querySelector(".warning-message");
 
 //Select Input field to verify emptiness:
 const input = document.querySelector("#search-field");
+//Select button-search:
+const searchButton = document.querySelector(".search-button");
 
 //Add event listner to the search button
-const searchButton = document
-  .querySelector(".search-icon")
-  .addEventListener("click", handleSearch);
+searchButton.addEventListener("click", handleSearchButtonClick);
+
+input.addEventListener("focus", handleSearchInputFocus);
 
 //Handle Search
-function handleSearch() {
-  console.log("clicked", typeof input);
-
+function handleSearchButtonClick() {
   if (input.value === "") {
-    warningMessage.style.display = "block";
-    input.classList.add("search-box--warning");
+
+    warningMessage.classList.remove("warning-message--hidden");
+    input.classList.add("search-input--warning");
+    return;
+    
   } else {
     warningMessage.style.display = "none";
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`)
@@ -30,6 +33,22 @@ function handleSearch() {
       });
     input.classList.remove("search-box--warning");
   }
+
+  warningMessage.classList.add("warning-message--hidden");
+  input.classList.remove("search-input--warning");
+
+  // The input.value can be used as input in the fetch call
+  fetchDictionary(input.value);
+}
+
+
+function handleSearchInputFocus() {
+  warningMessage.classList.add("warning-message--hidden");
+  input.classList.remove("search-input--warning");
+}
+
+function fetchDictionary(value) {
+  console.log("fetchDictionary", value);
 }
 
 
